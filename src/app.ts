23f34bfd838dcfb4f0ec3ffe1299b24a4c985748
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-17 11:53:53 +0200

    var / let /const

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-// Code goes here!
+const userName = 'Max';
+let age = 30;
+
+function add(a: number, b: number){
+    let result;
+    result = a + b;
+    return result;
+}
+
+// if(age > 20){
+//     // var isOld = true;
+//     let isOld = true;
+// }
+
+// console.log('isOld' , isOld); // Errore perchè isOld non esiste al di fuori dello scope del suo block
