fb43d8cfd6c85c91fd339511b23b51ef5986be69
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-17 12:03:05 +0200

    arrow functions

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 const userName = 'Max';
 let age = 30;
 
-function add(a: number, b: number){
-    let result;
-    result = a + b;
-    return result;
-}
+// function add(a: number, b: number){
+//     let result;
+//     result = a + b;
+//     return result;
+// }
 
 // if(age > 20){
 //     // var isOld = true;
@@ -13,3 +13,17 @@ function add(a: number, b: number){
 // }
 
 // console.log('isOld' , isOld); // Errore perchè isOld non esiste al di fuori dello scope del suo block
+
+// const add = (a: number, b: number) => {
+//     return a + b;
+// } 
+const add = (a: number, b: number) => a + b; //Forma breve se si ha una sola espressione
+console.log('add' , add(2, 5));
+
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+if(button){
+    button.addEventListener('click', event => console.log('event' , event))
+}
+printOutput(add(5, 2));
