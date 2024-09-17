5366684fa050a63bdf10bbde99c06cf34a8dffe0
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-17 12:56:12 +0200

    spread / rest

--- src/app.ts
+++ src/app.ts
@@ -17,13 +17,35 @@ let age = 30;
 // const add = (a: number, b: number) => {
 //     return a + b;
 // } 
-const add = (a: number, b: number) => a + b; //Forma breve se si ha una sola espressione
-console.log('add' , add(2, 5));
+// const add = (a: number, b: number = 1) => a + b; //Forma breve se si ha una sola espressione
+// console.log('add' , add(2, 5));
 
-const printOutput: (a: number | string) => void = output => console.log(output);
+// const printOutput: (a: number | string) => void = output => console.log(output);
 
-const button = document.querySelector('button');
-if(button){
-    button.addEventListener('click', event => console.log('event' , event))
-}
-printOutput(add(5, 2));
+// const button = document.querySelector('button');
+// if(button){
+//     button.addEventListener('click', event => console.log('event' , event))
+// }
+// printOutput(add(5));
+
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+activeHobbies.push(...hobbies);
+console.log('activeHobbies' , ...activeHobbies);
+
+const person = {
+    name: 'Max',
+    age: 30
+};
+console.log('person' , person);
+
+const copiedPerson = {...person};
+console.log('copiedPerson' , copiedPerson);
+const add = (...numbers: number[]) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0)
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log('addedNumbers' , addedNumbers);
